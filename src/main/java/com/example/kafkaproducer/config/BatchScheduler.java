136e8a8ffaa2e9766f33ae55bac21b2295c7f379
lllilllilllilili <hik3562@hufs.ac.kr>
2025-04-20 15:24:39 +0900

    배치 스케쥴러 추가

--- src/main/java/com/example/kafkaproducer/config/BatchScheduler.java
+++ src/main/java/com/example/kafkaproducer/config/BatchScheduler.java
@@ -1,4 +1,15 @@
 package com.example.kafkaproducer.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
 public class BatchScheduler {
+
+    @Scheduled(fixedDelay = 10000) //10초에 한번씩 작업 수행
+    public void runBatchJob() {
+
+    }
 }
