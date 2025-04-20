ee394c1437819697662decaf428e6d85b861526b
lllilllilllilili <hik3562@hufs.ac.kr>
2025-04-20 15:14:37 +0900

    카프카 Enable

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-
+@EnableKafka
 public class KafkaConfig {
     //todo(hig): https://github.com/dolphina02/logGenerator/blob/main/src/main/java/com/example/weblogproducer/config/KafkaConfig.java
     @Bean
